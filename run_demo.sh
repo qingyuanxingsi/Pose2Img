c6510bdede114c37263ef4853fba92859042bdba
doodleliang <doodleliang@tencent.com>
2023-12-31 15:26:41 +0800

    feat(ci): update demo

--- run_demo.sh
+++ run_demo.sh
@@ -1,5 +1,6 @@
+SPEAKER=luo
 python inference.py \
-   --cfg_path configs/yaml/xiang.yaml \
-   --name xiang_demo \
-   --npz_path target_pose/xiang/epoch0-DEMO-step1.npz \
-   --wav_path target_pose/xiang/epoch0-DEMO-step1.wav
+   --cfg_path configs/yaml/$SPEAKER.yaml \
+   --name ${SPEAKER}_demo \
+   --npz_path target_pose/$SPEAKER/epoch0-DEMO-step1.npz \
+   --wav_path target_pose/$SPEAKER/epoch0-DEMO-step1.wav
